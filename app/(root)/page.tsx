d9d3fadd7830b921d3c27254b52eb4fa5b7dc039
mounsif <mounsifderhay4@gmail.com>
2024-08-13 12:10:42 +0100

    Your commit message

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,14 +2,7 @@ import { SignIn, SignInButton } from '@clerk/nextjs'
 import React from 'react'
 
 const Home = () => {
-  return (
-    
-   <div>
-   
-    Home
-   </div>
-    
-  )
+  return <SignIn routing="hash" />
 }
 
 export default Home
